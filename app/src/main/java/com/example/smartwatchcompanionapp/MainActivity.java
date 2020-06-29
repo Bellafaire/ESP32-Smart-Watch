8859e56a5e636151070cb8a2a5cd52f0ef11a0b2
Matthew Bellafaire <matthew.bellafaire@gmail.com>
2020-06-28 20:44:50 -0400

    fixed broadcast reciever not working on android 7.0

--- app/src/main/java/com/example/smartwatchcompanionapp/MainActivity.java
+++ app/src/main/java/com/example/smartwatchcompanionapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,28 +16,42 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
     private TextView txtView;
+    private Button settings;
     private Button btn;
     private NotificationReceiver nReceiver;
 
+    String TAG = "inform";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "application starting");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         txtView= (TextView) findViewById(R.id.txt);
         btn = (Button) findViewById(R.id.button);
+        settings = (Button) findViewById(R.id.settings);
 
         nReceiver = new NotificationReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
-        registerReceiver(nReceiver,filter);
+
+       registerReceiver(nReceiver,filter);
 
         Intent i = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
         i.putExtra("command","list");
         sendBroadcast(i);
+
+    }
+
+
+    public void gotoSettings(View view){
+        Intent intent=new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        startActivity(intent);
     }
 
 
     public void updateText(View view){
+        Log.i(TAG, "update text button has been pressed");
         txtView.setText("");
         Intent i = new Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
         i.putExtra("command","list");
@@ -55,6 +70,7 @@ public class MainActivity extends Activity {
 
         @Override
         public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "onRecieve method callback received ");
             String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
             txtView.setText(temp);
         }
---
+++ app/src/main/java/com/example/smartwatchcompanionapp/NLService.java
@@ -0,0 +1,79 @@
+package com.example.smartwatchcompanionapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+public class NLService extends NotificationListenerService {
+
+    private String TAG = this.getClass().getSimpleName();
+    private NLServiceReceiver nlservicereciver;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        nlservicereciver = new NLServiceReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.kpbird.nlsexample.NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+        registerReceiver(nlservicereciver,filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nlservicereciver);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+
+        Log.i(TAG,"**********  onNotificationPosted");
+        Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText + "t" + sbn.getPackageName());
+        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "n");
+        sendBroadcast(i);
+
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification sbn) {
+        Log.i(TAG,"********** onNOtificationRemoved");
+        Log.i(TAG,"ID :" + sbn.getId() + "t" + sbn.getNotification().tickerText +"t" + sbn.getPackageName());
+        Intent i = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+        i.putExtra("notification_event","onNotificationRemoved :" + sbn.getPackageName() + "n");
+
+        sendBroadcast(i);
+    }
+
+    class NLServiceReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i("inform", "NLServiceReciever has received a broadcast");
+            if(intent.getStringExtra("command").equals("clearall")){
+                NLService.this.cancelAllNotifications();
+            }
+            else if(intent.getStringExtra("command").equals("list")){
+                Intent i1 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                i1.putExtra("notification_event","=====================");
+                sendBroadcast(i1);
+                int i=1;
+                for (StatusBarNotification sbn : NLService.this.getActiveNotifications()) {
+                    Intent i2 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                    i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "n");
+                    sendBroadcast(i2);
+                    i++;
+                }
+                Intent i3 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                i3.putExtra("notification_event","===== Notification List ====");
+                sendBroadcast(i3);
+
+            }
+
+        }
+    }
+
+}
